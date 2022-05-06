f1ca00361475d8953f376b01cc71673e8a632a10
Aaron Taylor <git@ataylor.io>
2022-05-05 22:33:37 -0400

    mat: add illustrative functions for einops

---
+++ mat/einops.go
@@ -0,0 +1,20 @@
+package mat
+
+// EinRearrange provides element re-ordering.
+// ref: https://einops.rocks/api/rearrange/
+func EinRearrange(operand Matrix, pattern string) Matrix {
+	return nil
+}
+
+// EinReduce provides a combination of reduction and re-ordering.
+// ref: https://einops.rocks/api/reduce/
+func EinReduce(operand Matrix, pattern string) Matrix {
+	return nil
+}
+
+// EinRepeat allows reordering and repeating elements in arbitrary
+// combinations.
+// ref: https://einops.rocks/api/repeat/
+func EinRepeat(operand Matrix, pattern string) Matrix {
+	return nil
+}
